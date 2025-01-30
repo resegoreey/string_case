747a589b3f6503dcea2df4d655e3f5af971616e7
RESEGO MOTLHASI <remotlhjhb024@student.wethinkcode.co.za>
2025-01-30 10:23:33 +0200

    Added functional code to convert strings into upper and lower case

---
+++ case.py
@@ -0,0 +1,12 @@
+#asks user for string input
+user_string = input("Enter a string: ")
+
+#convert the user provided string to upper case
+upper_case = user_string.upper()
+
+#converts the user provided string to lower case
+lower_case = user_string.lower()
+
+#displaying the results for both cases
+print(f"""Your message in uppercase: {upper_case}
+Your message in lowercase: {lower_case}""")
